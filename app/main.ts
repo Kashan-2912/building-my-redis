ae9265a4dd9e4ada38c7f7b50ecdf066afb27295
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-04-04 22:28:21 +0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,10 +7,19 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection\
   connection.on("data", (data: Buffer) => {
-    connection.write("+PONG\r\n");
+    const input = data.toString();
+    
+    const match = input.match("/\*2\r\n$\d+\r\n([Ee][Cc][Hh][Oo])\r\n\$(\d+)\r\n(.*)\r\n/ ,")
+
+    if(match) {
+      const arg = match[3];
+      connection.write(`$${arg.length}\r\n${arg}\r\n`);
+      return;
+    }
+
+    connection.write(`+PONG\r\n`);
   })
 
-  
 });
 //
 server.listen(6379, "127.0.0.1");
