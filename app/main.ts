b712b22e71703589bb88f1666294c10fc0f2db33
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2026-04-05 20:27:17 +0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -137,9 +137,9 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
         startIndex = listName.length - 1 + startIndex;
         stopIndex = listName.length - 1 + stopIndex;
       } else if (startIndex < 0 && stopIndex >= 0) {
-        startIndex = listName.length - 1 + startIndex;
+        startIndex = listName.length + startIndex;
       } else if (startIndex >= 0 && stopIndex < 0) {
-        stopIndex = listName.length - 1 + stopIndex;
+        stopIndex = listName.length + stopIndex;
       } else if (Math.abs(startIndex) > listName.length) {
         startIndex = 0;
       }
